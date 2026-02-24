c090cf1b74c3e8b3d1260e25587dd089261ddf0d
haripriya-reddi07 <hr2566@srmist.edu.in>
2026-02-24 15:43:34 +0530

    haripriya

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,7 +1,24 @@
+
 public class PalindroneCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrone Checker App management system");
+
+        String input = "madam";
+        boolean isPalindrone = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrone = false;
+                break;
+            }
+        }
+        if (isPalindrone) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+
+
+
     }
-
-
 }
